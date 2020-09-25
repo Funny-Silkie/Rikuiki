9bca8972af26618bf7f282059577fb391585c2ab
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-09-25 16:12:06 +0900

    split file

--- Rikuiki_Plant/Program.cs
+++ Rikuiki_Plant/Program.cs
@@ -1,7 +1,6 @@
 using Rikuiki;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.IO;
 using System.Text;
 
@@ -105,52 +104,6 @@ namespace Rikuiki_Plant
                 writer.WriteLine($"Sum,{baSum * 1.1m},{baSum},,{entryArray.Sum(x => x.dphAve)},{entryArray.Sum(x => x.dbhMax)},{countSum},,{countSum / 1.1m}");
                 Console.WriteLine("Done");
             }
-        }
-    }
-    class TreeInfo
-    {
-        private readonly List<decimal> dbhs = new List<decimal>();
-        private readonly HashSet<string> tags = new HashSet<string>();
-        public int Count => tags.Count;
-        public ReadOnlyCollection<decimal> DBHs { get; }
-        public string Name { get; }
-        public IEnumerable<string> Tags
-        {
-            get
-            {
-                foreach (var tag in tags) yield return tag;
-            }
-        }
-        public TreeInfo(string name)
-        {
-            Name = name;
-            DBHs = dbhs.AsReadOnly();
-        }
-        public void Add(string tag, decimal dbh)
-        {
-            dbhs.Add(dbh);
-            tags.Add(tag);
-        }
-        public bool Calc(out decimal ba, out decimal dbhMax, out decimal dbhAve)
-        {
-            if (dbhs.Count == 0)
-            {
-                ba = default;
-                dbhMax = default;
-                dbhAve = default;
-                return false;
-            }
-            dbhMax = decimal.MinValue;
-            var sum = 0m;
-            ba = 0m;
-            foreach (var current in dbhs)
-            {
-                if (dbhMax < current) dbhMax = current;
-                sum += current;
-                ba += current * current / 4m / (decimal)Math.PI / 10000m;
-            }
-            dbhAve = sum / dbhs.Count;
-            return true;
         }
     }
 }
---
+++ Rikuiki_Plant/TreeInfo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Rikuiki_Plant
+{
+    internal class TreeInfo
+    {
+        private readonly List<decimal> dbhs = new List<decimal>();
+        private readonly HashSet<string> tags = new HashSet<string>();
+        public int Count => tags.Count;
+        public ReadOnlyCollection<decimal> DBHs { get; }
+        public string Name { get; }
+        public IEnumerable<string> Tags
+        {
+            get
+            {
+                foreach (var tag in tags) yield return tag;
+            }
+        }
+        public TreeInfo(string name)
+        {
+            Name = name;
+            DBHs = dbhs.AsReadOnly();
+        }
+        public void Add(string tag, decimal dbh)
+        {
+            dbhs.Add(dbh);
+            tags.Add(tag);
+        }
+        public bool Calc(out decimal ba, out decimal dbhMax, out decimal dbhAve)
+        {
+            if (dbhs.Count == 0)
+            {
+                ba = default;
+                dbhMax = default;
+                dbhAve = default;
+                return false;
+            }
+            dbhMax = decimal.MinValue;
+            var sum = 0m;
+            ba = 0m;
+            foreach (var current in dbhs)
+            {
+                if (dbhMax < current) dbhMax = current;
+                sum += current;
+                ba += current * current / 4m / (decimal)Math.PI / 10000m;
+            }
+            dbhAve = sum / dbhs.Count;
+            return true;
+        }
+    }
+}
